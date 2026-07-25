344f079ea3bc7ccab6fb497992db0f839e4a619f
cqygfxgfst1823 <2609481667@qq.com>
2026-07-25 08:48:03 +0800

    Fix Fibonacci function cache initialization
    Initialize Fibonacci cache and set loop condition.

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -5,10 +5,14 @@
 // READ: 纯函数 <https://zh.wikipedia.org/wiki/%E7%BA%AF%E5%87%BD%E6%95%B0>
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
+   static unsigned long long cache[96] = {0, 1};
+    static bool initialized = false;             
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+   if (!initialized) {
+        for (int j = 2; j < 96; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];
+        }
+        initialized = true;// 设置标识为已初始化
     }
     return cache[i];
 }
