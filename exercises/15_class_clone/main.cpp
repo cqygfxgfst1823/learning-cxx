8a40f19d9f9b284d6c3c1fe4a4267b5a8e77273d
cqygfxgfst1823 <2609481667@qq.com>
2026-07-31 14:03:34 +0800

    finish 15
    Implemented dynamic constructor, copy constructor, and destructor for DynFibonacci class. Enhanced Fibonacci calculation with caching.

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -10,19 +10,41 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]{}), cached(0) { if (capacity > 0) {
+            cache[0] = 0;// F(0)
+            cached++;
+        }
+        if (capacity > 1) {
+            cache[1] = 1;// F(1)
+            cached++;
+        }}
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+   DynFibonacci(DynFibonacci const &other) : cache(new size_t[other.cached]), cached(other.cached) {
+        // 复制缓存数据
+        for (int i = 0; i < cached; ++i) {
+            cache[i] = other.cache[i];
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci(){
+        delete[] cache;// 释放动态分配的缓存
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+       if (i < cached) {
+            return cache[i];// 返回缓存的值
         }
+
+        // 计算并填充缓存
+        for (int j = cached; j <= i; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];// 填充缓存
+        }
+
+        // 更新已缓存的最大索引
+        cached = i + 1;
         return cache[i];
     }
 
