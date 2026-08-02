5516b7fc9ea14858945f7a7b00a2d5e6339801f9
cqygfxgfst1823 <2609481667@qq.com>
2026-08-02 09:04:08 +0800

    finish 23

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -11,6 +11,13 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+       std::memcpy(shape, shape_, N * sizeof(unsigned int));// 填入正确的 shape
+        size = 1;
+        for (unsigned int i = 0; i < N; ++i) {
+            size *= shape[i];// 计算 size
+        }
+
+        
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
